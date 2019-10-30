3522f15a0455a887222014f1bbca2d6ef7de77b3
gary-exe <56901369+gary-exe@users.noreply.github.com>
2019-10-30 22:01:46 +1100

    Add files via upload

---
+++ true false practise.py
@@ -0,0 +1,11 @@
+is_hot = False
+is_cold = False
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif is_cold:
+    print("It's a cold day")
+    print("Wear lots of warm clothes")
+else:
+    print("It's a lovely day")
+print("Enjoy your day")
