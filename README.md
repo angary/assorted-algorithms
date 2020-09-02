264d780e45e1f348f57254788a4a91a773d44eb8
Gy74S <garysun0451@gmail.com>
2020-09-02 15:44:34 +1000

    Cleaning up code in magic knights tour
    Small changes to make it more readable

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Hello Gary in the future, here's your cringy code.
 |Program|Info|
 |---|---|
 |<span>true false practise.py</span>|First piece of code I uploaded to github in this previously private repo, held here for SenTimEnTaL vAluE.|
-|magic_knights_tour.py|A program that aims to create a semi magic square from a knight's tour. Warnsdorff's algorithm combined with my attempts at a magic square heuristic / quad backtracking.|
+|magic_knights_tour.py|A program that generates a magic knights tour using regular quartes, Warnsdorff's algorithm and quad backtracking. |
--- magic_knights_tour.py
+++ magic_knights_tour.py
@@ -1,9 +1,7 @@
 # Program to find magic square knights tour
-# Doesn't consider diagonals, because I'm not sure if they exist for 8 x 8
-# In fact it doesn't even work (idk how long it'll take to find a solution)
-# Uses Warnsdorff's algo, but augmented to suit magic square conditions
-# It also alternates/ backtracks bewteen quads using a stack every 4 moves
-
+# Utilises backtracking with Warnsdorff algorithm 
+# Generates magic square made of regular quartes with quad back tracking
+# Enter input like 'a1' or 'h4'
 
 dx = [2, 1, -1, -2, -2, -1, 1, 2]
 dy = [1, 2, 2, 1, -1, -2, -2, -1]
@@ -12,55 +10,55 @@ dy = [1, 2, 2, 1, -1, -2, -2, -1]
 # Driver code
 def main():
 	source = input("Starting position: ")
-	src = [int(ord(source[0].lower()) - int(ord('a'))), int(source[1]) - 1]
+	y, x = int(ord(source[0].lower()) - int(ord('a'))), int(source[1]) - 1
 	b = [[0 for j in range(8)] for i in range(8)]
-	y, x = src[0], src[1]
 	b[y][x] = 1
 	printBoard(b)
 	maxStack = 16
-	if ((y == 2 or y == 5) and (x == 3 or x == 4)) or ((y == 3 or y == 4) and (x == 2 or x == 5)):
+	if (((y == 2 or y == 5) and (x == 3 or x == 4)) or
+		((y == 3 or y == 4) and (x == 2 or x == 5))):
 		maxStack = 32
-	if solve(b, src, 1, [quadHash(y, x)], False, maxStack):
+	if solve(b, [y, x], 1, [quadHash(y, x)], False, maxStack):
 		printBoard(b)
 	else:
 		print("Could not find solution")
 
 
 # Backtracking solve
-def solve(b, pos, t, qStack, backtrack, maxStack):
-	if t == 64:
+def solve(b, pos, turn, qStack, backtrack, maxStack):
+	if turn == 64:
 		return True
 	if backtrack and len(qStack) == 0: 
 		backtrack = False
 	if not backtrack and len(qStack) == maxStack: 
 		backtrack = True
 	quads = findQuad(qStack, backtrack)
-	pq = heuristic(b, pos, t + 1, quads)
+	pq = heuristic(b, pos, turn + 1, quads)
 	for i in range(len(pq)):
 		x = pos[1] + dx[pq[i]]
 		y = pos[0] + dy[pq[i]]
-		b[y][x] = t + 1
+		b[y][x] = turn + 1
 		newStack = qStack.copy()
 		if not backtrack: 
 			newStack.append(quadHash(y, x))
-		if solve(b, [y, x], t + 1, newStack, backtrack, maxStack):
+		if solve(b, [y, x], turn + 1, newStack, backtrack, maxStack):
 			return True
 		b[y][x] = 0
 	return False
 
 
 # Return priority queue of directions to go to
-def heuristic(b, pos, t, quads):
+def heuristic(b, pos, turn, quads):
 	d = []
 	for i in range(8):
 		x = pos[1] + dx[i]
 		y = pos[0] + dy[i]
-		if isMagic(b, y, x, t) and quadHash(y, x) in quads:
+		if isMagic(b, y, x, turn) and quadHash(y, x) in quads:
 			degree = 0
 			for j in range(8):
 				nY = y + dy[j]
 				nX = x + dx[j]
-				if isMagic(b, nY, nX, t + 1):
+				if isMagic(b, nY, nX, turn + 1):
 					degree += 1
 			d.append([degree, i])
 	d.sort(key = lambda i:i[0])
@@ -82,7 +80,7 @@ def findQuad(qStack, backtrack):
 def printBoard(b):
     for i in range(8):
         for j in range(8):
-            print("%2d " % b[i][j], end="")
+            print("%2d " % b[i][j], end = "")
         print()
     print()
 
@@ -93,22 +91,22 @@ def quadHash(y, x):
 
 
 # Check if the new move satisfies magic condition
-def isMagic(b, y, x, t):
+def isMagic(b, y, x, turn):
 	if x < 0 or x > 7 or y < 0 or y > 7 or b[y][x] != 0:
 		return False
 	tmpBoard = [row[:] for row in b]
-	tmpBoard[y][x] = t
+	tmpBoard[y][x] = turn
 	halfRow = tmpBoard[y][4 * (x > 3) : 4 * (1 + (x > 3))]
-	for i in halfRow:
-		if i > 0 and i != t and (i - 1) // 4 == (t - 1) // 4:
+	for val in halfRow:
+		if val > 0 and val != turn and (val - 1) // 4 == (turn - 1) // 4:
 			return False
 	halfCol = [tmpBoard[i][x] for i in range(4 * (y > 3), 4 * (1 + (y > 3)))]
-	for i in halfCol:
-		if i > 0 and i != t and (i - 1) // 4 == (t - 1) // 4:
+	for val in halfCol:
+		if val > 0 and val != turn and (val - 1) // 4 == (turn - 1) // 4:
 			return False
-	r = tmpBoard[y]
-	c = [tmpBoard[i][x] for i in range(8)]
-	if (min(r) > 0 and sum(r) != 260) or (min(c) > 0 and sum(c) != 260):
+	row = tmpBoard[y]
+	col = [tmpBoard[i][x] for i in range(8)]
+	if (min(row) > 0 and sum(row) != 260) or (min(col) > 0 and sum(col) != 260):
 		return False
 	return True
 
