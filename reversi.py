47efe41e02766739f9bb8573027253e32217b5be
Gy74S <garysun0451@gmail.com>
2020-09-12 22:42:27 +1000

    Saving before i implement negamax

--- reversi.py
+++ reversi.py
@@ -1,17 +1,24 @@
 # Contains the code for implementation of reversi and minimax AI
 # REPLACED "B" WITH "." FOR VISIBILTY
-# TODO: Add err check for input, alpha beta pruning, heuristic
+# TODO:
+# - Improve heuristic
+# - Transposition table
+# - Negamax
+# - Beam search
 
 import math
 import copy
 import time
 
-# Currently the weights is predefined, hopefully can make an algo to determine
-SIZE = 4
+SIZE = 8
 MAX_TURN = (SIZE ** 2) - 3
-colour = ["W", "."]
-dy = [1, 1, 0, -1, -1, -1, 0, 1]
-dx = [0, 1, 1, 1, 0, -1, -1, -1]
+BLANK = "_"
+COLOUR = ["W", "."]
+DY = [1, 1, 0, -1, -1, -1, 0, 1]
+DX = [0, 1, 1, 1, 0, -1, -1, -1]
+
+weights = [[0 for j in range(SIZE)] for i in range(SIZE)]
+checked = 0
 
 
 # Object to contain game data and methods
@@ -19,15 +26,15 @@ dx = [0, 1, 1, 1, 0, -1, -1, -1]
 class Game(object):
 
 	def __init__(self):
-		self.b = [["_" for j in range(SIZE)] for i in range(SIZE)]
+		self.b = [[BLANK for j in range(SIZE)] for i in range(SIZE)]
 		self.flip = [[[] for i in range(SIZE)] for j in range(SIZE)]
 		self.turn = 0
 		mid_s = (SIZE // 2) - 1
 		mid_b = SIZE // 2
-		self.b[mid_s][mid_s] = colour[0]
-		self.b[mid_b][mid_b] = colour[0]
-		self.b[mid_s][mid_b] = colour[1]
-		self.b[mid_b][mid_s] = colour[1]
+		self.b[mid_s][mid_s] = COLOUR[0]
+		self.b[mid_b][mid_b] = COLOUR[0]
+		self.b[mid_s][mid_b] = COLOUR[1]
+		self.b[mid_b][mid_s] = COLOUR[1]
 
 	# Print out the board
 	def print_board(self):
@@ -50,33 +57,33 @@ class Game(object):
 		whites = 0
 		blacks = 0
 		for y in range(SIZE):
-			whites += self.b[y].count(colour[0])
-			blacks += self.b[y].count(colour[1])
+			whites += self.b[y].count(COLOUR[0])
+			blacks += self.b[y].count(COLOUR[1])
 		if whites == 0 or blacks == 0:
 			return True
 		return False
 
 	# Return the number of white and black squares
 	def find_score(self):
-		score = {colour[0]: 0, colour[1]: 0}
+		score = {COLOUR[0]: 0, COLOUR[1]: 0}
 		for y in range(SIZE):
 			for x in range(SIZE):
-				if self.b[y][x] == colour[0]:
-					score[colour[0]] += 1
-				elif self.b[y][x] == colour[1]:
-					score[colour[1]] += 1
+				if self.b[y][x] == COLOUR[0]:
+					score[COLOUR[0]] += 1
+				elif self.b[y][x] == COLOUR[1]:
+					score[COLOUR[1]] += 1
 		return score
 
 	# Register the player's chosen location on the board
 	def go(self, y, x, player):
-		curr_colour = colour[player]
-		oth_colour = colour[(player + 1) % 2]
+		curr_colour = COLOUR[player]
+		oth_colour = COLOUR[(player + 1) % 2]
 		self.b[y][x] = curr_colour
 		flip_dirs = self.find_flips(y, x, oth_colour, curr_colour)[0]
 		for dir_index in flip_dirs:
 			for mul in range(1, SIZE):
-				nY = y + mul * dy[dir_index]
-				nX = x + mul * dx[dir_index]
+				nY = y + mul * DY[dir_index]
+				nX = x + mul * DX[dir_index]
 				if not self.in_limits(nY, nX) or self.b[nY][nX] == curr_colour:
 					break
 				self.b[nY][nX] = curr_colour
@@ -97,10 +104,10 @@ class Game(object):
 
 	# Check if a square is valid
 	def valid(self, y, x, player):
-		if self.b[y][x] != "_":
+		if self.b[y][x] != BLANK:
 			return 0
-		curr_colour = colour[player]
-		oth_colour = colour[(player + 1) % 2]
+		curr_colour = COLOUR[player]
+		oth_colour = COLOUR[(player + 1) % 2]
 		flip_dirs, flipped = self.find_flips(y, x, oth_colour, curr_colour)
 		return flipped
 
@@ -108,15 +115,15 @@ class Game(object):
 	def find_flips(self, y, x, oth_colour, curr_colour):
 		flipped = 0
 		flip_dirs = []
-		for dir_index in range(len(dx)):
-			if not self.in_limits(y + dy[dir_index], x + dx[dir_index]):
+		for dir_index in range(len(DX)):
+			if not self.in_limits(y + DY[dir_index], x + DX[dir_index]):
 				continue
-			if self.b[y + dy[dir_index]][x + dx[dir_index]] == oth_colour:
+			if self.b[y + DY[dir_index]][x + DX[dir_index]] == oth_colour:
 				curr_flipped = 0
 				for mul in range(1, SIZE):
-					nY = y + mul * dy[dir_index]
-					nX = x + mul * dx[dir_index]
-					if not self.in_limits(nY, nX) or self.b[nY][nX] == "_":
+					nY = y + mul * DY[dir_index]
+					nX = x + mul * DX[dir_index]
+					if not self.in_limits(nY, nX) or self.b[nY][nX] == BLANK:
 						break
 					if self.b[nY][nX] == curr_colour:
 						flip_dirs.append(dir_index)
@@ -126,44 +133,57 @@ class Game(object):
 		return flip_dirs, flipped
 
 
-
 # Driver code + code to take human input
 ################################################################################
 def main():
+	global weights
+
+	# Calculate the weights
+	for i in range(SIZE):
+		for j in range(SIZE):
+			weights[i][j] += max(i, SIZE - 1 - i)
+			weights[i][j] += max(j, SIZE - 1 - j)
+
 	print("Game started")
 	print("Enter positions as <letter><number>, i.e. a1")
 	game = Game()
 	game.turn = 1
 	game.print_board()
+	start_time = time.time()
 
 	# Game loop
 	while not game.over():
 		player = game.turn % 2
 		print("Valid moves is", game.find_valid(player))
-
+		global checked
 		# Take human input
 		if player == 1:
-			# y, x = take_turn(game, player)
-			# game.go(y, x, player)
-			start_time = time.time()
-			best = minimax(game, player, player, -math.inf, math.inf)
-			game.go(best[1], best[2], player)
-			print("Prediction for best score:", best[0])
-			print("Time taken:", time.time() - start_time)	
+			y, x = take_turn(game, player)
+			game.go(y, x, player)
+			# checked = 0
+			# start_time = time.time()
+			# best = negamax(game, player, player, -math.inf, math.inf, 6)
+			# game.go(best[1], best[2], player)
+			# print("Prediction for best score:", best[0])
+			# print("Time taken: {0:.2}".format(time.time() - start_time))
+			# print("checked: ", checked)
 
 		# Take AI input
 		else:
-			start_time = time.time()
-			best = minimax(game, player, player, -math.inf, math.inf)
+			checked = 0
+			# start_time = time.time()
+			best = negamax(game, player, player, -math.inf, math.inf, 4)
 			game.go(best[1], best[2], player)
-			print("Prediction for best score:", best[0])
-			print("Time taken:", time.time() - start_time)
+			# print("Prediction for best score:", best[0])
+			# print("Time taken: {0:.2}".format(time.time() - start_time))
+			print("checked: ", checked)
 
 		game.print_board()
 		print(game.find_score())
 		print("Turn is", game.turn)
 	
 	print("Game over")
+	print("Time taken: {}" .format(time.time() - start_time))
 	game.print_board()
 	print(game.find_score())
 
@@ -171,7 +191,7 @@ def main():
 # Take the input from human player
 def take_turn(game, player):
 	while True:
-		print("Currently", colour[player])
+		print("Currently", COLOUR[player])
 		try:
 			loc = input("Choose position: ")
 			x, y = int(ord(loc[0].lower()) - int(ord('a'))), int(loc[1]) - 1
@@ -185,19 +205,25 @@ def take_turn(game, player):
 
 # Ai code
 ################################################################################
-def minimax(game, player, maximise, alpha, beta):
-	if game.over():
-		return (game.find_score()[colour[maximise]], 0, 0)
+def negamax(game, player, maximise, alpha, beta, depth):
+	if game.over() or depth == 0:
+		score = game.find_score()[COLOUR[player]]
+		score -= (game.turn) // 2
+		return (heuristic_weight(game, player), 0, 0)
+	depth -= 1
 
+	global checked
+	checked += 1
 	best_eval = -math.inf if player == maximise else math.inf
 	valid_moves = game.find_valid(player)
+	valid_moves = heuristic_sort(game, player, valid_moves)
 	best_y, best_x = valid_moves[0]
 
 	for y, x in valid_moves:
 		new_game = copy.deepcopy(game)
 		new_game.go(y, x, player)
 		player = (player + 1) % 2
-		new_eval = minimax(new_game, player, maximise, alpha, beta)
+		new_eval = negamax(new_game, player, maximise, alpha, beta, depth)
 
 		if player == maximise:
 			alpha = max(alpha, new_eval[0])
@@ -216,6 +242,27 @@ def minimax(game, player, maximise, alpha, beta):
 			break
 
 	return best_eval, best_y, best_x
+
+
+def heuristic_sort(game, player, valid_moves):
+	sorted_moves = []
+	for move in valid_moves:
+		y, x = move
+		sorted_moves.append({"move": move, "weight": weights[y][x]})
+	
+	sorted_moves.sort(key = lambda x:x["weight"], reverse = True)
+
+	return [move["move"] for move in sorted_moves]
+
+
+def heuristic_weight(game, player):
+	score = 0
+	maximise = COLOUR[player]
+	for y in range(SIZE):
+		for x in range(SIZE):
+			if game.b[y][x] == maximise:
+				score += weights[y][x]
+	return score
 
 
 if __name__ == "__main__":
