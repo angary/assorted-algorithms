91bccd5a047182fed83403d10ed488d531fd9940
Gy74S <garysun0451@gmail.com>
2020-11-08 17:52:46 +1100

    Replaced graph and vertex class with 2D dict

--- markov_chain_text.py
+++ markov_chain_text.py
@@ -1,45 +1,11 @@
 # Program to generate text from given text using markov chain
-# Uses a very bare minimum graph ADT
 
 
 import random
 import string
 
 
-class Vertex(object):
-	def __init__(self):
-		self.neighbours = {}
-
-	def add_edge(self, dest):
-		if dest in self.neighbours:
-			self.neighbours[dest] += 1
-		else:
-			self.neighbours[dest] = 1
-
-	def next_vertices(self):
-		return self.neighbours
-
-
-class Graph(object):
-	def __init__(self):
-		self.g = {}
-
-	def add_vertex(self, word):
-		self.g[word] = Vertex()
-	
-	def add_edge(self, src, dest):
-		if src not in self.g:
-			self.add_vertex(src)
-		self.g[src].add_edge(dest)
-
-	def random_vertex(self):
-		return random.choice(list(self.g))
-
-	def next_vertices(self, src):
-		if src in self.g:
-			return self.g[src].next_vertices()
-		else:
-			return {}
+word_graph = {}
 
 
 def main():
@@ -52,25 +18,42 @@ def main():
 			for w in line.split():
 				w.lower().translate(str.maketrans('', '', string.punctuation))
 				words.append(w)
-	
+
 	# Transfer words to a graph
-	g = Graph()
 	for i in range(len(words) - 1):
-		g.add_edge(words[i], words[i + 1])
-	
+		curr = words[i]
+		next = words[i + 1]
+		if curr in word_graph.keys():
+			if next in word_graph[curr]:
+				word_graph[curr][next] = word_graph[curr][next] + 1
+			else:
+				word_graph[curr][next] = 1
+		else:
+			word_graph[curr] = {}
+			word_graph[curr][next] = 1
+
 	# Generate text
 	word_count = int(input("Enter length of generated text: "))
-	curr = g.random_vertex()
+
+	# Choose random starting word in dict
+	curr = random.choice(list(word_graph.keys()))
 	for _ in range(word_count):
+
+		# Loop through choosing next word
 		print(curr, end = " ")
-		next_words_dict = g.next_vertices(curr)
+		next_words_dict = word_graph[curr]
+
+		# If it can continue the chain
 		if next_words_dict:
 			next_words = list(next_words_dict.keys())
 			weights = list(next_words_dict.values())
 			curr = random.choices(next_words, weights)[0]
+		
+		# Else just choose another random word
 		else:
-			curr = g.random_vertex()
+			curr = random.choice(list(word_graph.keys()))
 	print()
+
 
 if __name__ == "__main__":
 	main()
